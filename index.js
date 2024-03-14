78f10aca3d11ca7b32d1588be43fa1c3f57127ea
“[firstname <sanskarsrs9@gmail.com>
2024-03-14 21:40:23 +0530

    4

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["https://front-mars-one.vercel.app/", "http://localhost:3000"],
+        origin: ["https://front-mars-one.vercel.app", "http://localhost:3000"],
         // origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
